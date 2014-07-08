cd9ad439a991384ae04d003e33f1405e0ea18850
Olivia Cheng <olivia@heyitsolivia.com>
2014-07-08 02:09:15 -0400

    add sass to grunt default tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,8 +84,8 @@ module.exports = function(grunt) {
 	grunt.task.loadTasks('./builder/');
 
 	//if you choose to use scss, or any preprocessor, you can add it here
-	grunt.registerTask('default', ['clean', 'concat', 'patternlab', /*'sass',*/ 'copy']);
+	grunt.registerTask('default', ['clean', 'concat', 'patternlab', 'sass', 'copy']);
 
 	//travis CI task
-	grunt.registerTask('travis', ['clean', 'concat', 'patternlab', /*'sass',*/ 'copy', 'qunit'])
+	grunt.registerTask('travis', ['clean', 'concat', 'patternlab', 'sass', 'copy', 'qunit'])
 };
